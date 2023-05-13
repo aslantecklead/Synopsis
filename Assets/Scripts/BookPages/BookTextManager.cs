d40500ee9290d1cf8f32abef68b6816a59826a6d
aslantecklead <97795768+aslantecklead@users.noreply.github.com>
2023-05-13 20:47:58 +0300

    Stprage

--- Assets/Scripts/BookPages/BookTextManager.cs
+++ Assets/Scripts/BookPages/BookTextManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5067be3e9314180011fba7ce654df9fd67b4bf78fe93b866a577af7349b02296
-size 1532
+oid sha256:b7c2b6c495fa967ef48801e405a0eed245020ae3814fe974554e9b7876a858a3
+size 1380
