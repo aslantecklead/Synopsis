5b20393bd8e4eb1fd9a705ffb2bbe31182b380f6
aslantecklead <97795768+aslantecklead@users.noreply.github.com>
2023-05-18 11:38:48 +0300

    слова в дебаге

--- Assets/Scripts/BookPages/BookContents.cs
+++ Assets/Scripts/BookPages/BookContents.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e2d37e567fa80b90826e2c8adac14637fe6c1325eb0a58ddf3d98d0408b33ae
-size 2901
+oid sha256:2e72dc1d83ade3d949d075aeee48f504b80ef0e25a08acbc7df95598c71e0f9d
+size 2918
