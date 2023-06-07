829076860e7dcd72cd46996fd9f6759684ab6918
aslantecklead <97795768+aslantecklead@users.noreply.github.com>
2023-06-07 16:32:55 +0300

    1

--- Assets/Scripts/Jsonmanager.cs
+++ Assets/Scripts/Jsonmanager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fe47a0430d3c3f2c490b9f85e47da0b2a57e0ae6ed579e19291890ef0b8862f
-size 2961
+oid sha256:892efa7d7b85cae942b25dd63e050c1e65265e5b617faeaedf957839ea67922a
+size 2719
